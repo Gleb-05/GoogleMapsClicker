1889cae305c8eadca693d09d92ab7be02a34ddc3
GLEB <gms.4job@gmail.com>
2026-07-18 02:10:36 +0300

    add BasicFrame to make content scrollable in edit_configs

---
+++ tk_app_frames/BasicFrame.py
@@ -0,0 +1,75 @@
+import tkinter as tk
+
+class BasicFrame:
+    '''Vertically resizable frame with header, scrollable body, and footer'''
+    
+    MAX_HEIGHT = 555
+    MAX_WIDTH = 555
+
+    def __init__(self, root: tk.Tk):
+        self.root = root
+        self.header = tk.Frame(root)
+        self.header.pack(fill="x", expand=False, padx=10, pady=10)
+        middle_frame, self.body = self._create_body()
+        middle_frame.configure(borderwidth=4, relief=tk.GROOVE)
+        middle_frame.pack(fill="both", expand=True, padx=10)
+        self.footer = tk.Frame(root)
+        self.footer.pack(fill="x", expand=False, padx=10, pady=10)
+
+
+    def _create_body(self) -> tuple[tk.Frame, tk.Frame]:
+        '''
+        Return 
+        `middle_frame` - convenience container for scrollbar and such, 
+        and `body_frame` - the actual body that has to be filled with content
+        '''
+        # https://stackoverflow.com/questions/3085696/adding-a-scrollbar-to-a-group-of-widgets-in-tkinter/3092341
+        
+        def onFrameConfigure(canvas: tk.Canvas):
+            '''Reset the scroll region to encompass the inner frame'''
+            canvas.configure(scrollregion=canvas.bbox("all"))
+
+        middle_frame = tk.Frame(self.root, height=0)
+
+        canvas = tk.Canvas(middle_frame, height=0, borderwidth=0, background="#8da0c5")
+        body_frame = tk.Frame(canvas, height=0, background="#ffffff")
+        vsb = tk.Scrollbar(middle_frame, orient="vertical", command=canvas.yview)
+        canvas.configure(yscrollcommand=vsb.set)
+
+        vsb.pack(side="right", fill="y")
+        canvas.pack(side="left", fill="both", expand=True)
+        # frame.pack(fill="both", expand=True)
+        canvas.create_window((0,0),window=body_frame, anchor="nw")
+
+        self._vsb = vsb  # crutch needed for update_root_geometry
+
+        body_frame.bind("<Configure>", lambda event, canvas=canvas: onFrameConfigure(canvas))
+        return middle_frame, body_frame
+    
+
+    def update_root_geometry(self):
+        '''
+        After BasicFrame contents are updated, 
+        resize window to fit `body.winfo_reqwidth` 
+        and combined height of header, body and footer.
+        
+        `root.resizable(False, True)` is called to make the width locked and the height resizable.
+        '''
+        self.root.update_idletasks()
+
+        width = (
+            self.body.winfo_reqwidth() + self._vsb.winfo_reqwidth() 
+            + 10*2 + 4*2 + 4 # middle padx, middle borders, small margin
+        )
+        width = min(width, BasicFrame.MAX_WIDTH)
+
+        height = (
+            self.body.winfo_reqheight() + self.header.winfo_reqheight() + self.footer.winfo_reqheight() 
+            + 10 * 4 + 4 * 2 + 4 # header and footer pady, middle borders, small margin
+        )
+        height = min(height, BasicFrame.MAX_HEIGHT)
+        
+        # print(f"{width} x {height}")
+
+        self.root.geometry(f"{width}x{height}")
+        self.root.resizable(False, True)
--- tk_app_frames/edit_configs.py
+++ tk_app_frames/edit_configs.py
@@ -1,54 +1,63 @@
 
 import tkinter as tk
+from tk_app_frames.BasicFrame import BasicFrame
 from dataclasses import Field
-from gui.core_configs import C_search
+# from gui.core_configs import C_search
+import usr_get_area_img  # crutch to get all necessary configs
 from config_registry import get_tk_fields, _config_register, ConfigRegistryMixin, ConfigTkMeta
 
 
-def make_config_frame(config: ConfigRegistryMixin, root: tk.Tk):
+def make_config_frame(config: ConfigRegistryMixin, master: tk.Misc):
     tk_fields = get_tk_fields(config)
-    config_frame = tk.Frame(root)
+    config_frame = tk.Frame(master)
     for field in tk_fields:
-        make_field_entry(field, config_frame).pack(fill=tk.X, expand=True)
+        make_field_entry(field, config_frame).pack(fill=tk.X, expand=True, padx=10, pady=10)
     return config_frame
 
 def make_field_entry(field: Field, master: tk.Misc):
     field_frame = tk.Frame(master)
+    tk.Frame(field_frame, height=1, background="gray").pack(fill=tk.X, expand=True, pady=(0,5))
     meta: ConfigTkMeta = field.metadata.get(ConfigTkMeta.KEY)
     tk.Entry(field_frame).pack(anchor=tk.W)
-    tk.Label(field_frame, text=field.name, wraplength=400).pack(anchor=tk.W)
-    tk.Label(field_frame, text=meta.doc, wraplength=400).pack(anchor=tk.W)
+    kw_label_make = {"master": field_frame, "wraplength": 400, "justify":"left"}
+    tk.Label(text=field.name, **kw_label_make).pack(anchor=tk.W)
+    tk.Label(text=meta.doc,   **kw_label_make).pack(anchor=tk.W)
     return field_frame
 
-class EditConfigsFrame:
+class EditConfigsFrame(BasicFrame):
+    '''See and change individual fields of configs'''
     def __init__(self, root: tk.Tk):
+        super().__init__(root)
         self.root = root
         self.root.title("Prepare")
         self.W, self.H = root.winfo_screenwidth(), root.winfo_screenheight()
         self.root.minsize(300, 100)
         self.root.attributes("-topmost", True)
 
-        self.configs = {C_search.REGISTER_KEY: make_config_frame(C_search, root)}
-        self.config_names = list(self.configs.keys())
-        self.current_config_name = C_search.REGISTER_KEY
-        self.current_config_strvar = tk.StringVar(value=self.current_config_name)
-        tk.OptionMenu(root, self.current_config_strvar, *self.config_names, command=self.show_frame).pack(pady=10)
-        self.configs[self.current_config_name].pack(fill="both")
-        
-        # instruction = "Press NumLk (or alt+f2) after moving your cursor to a suitable position."
-        # tk.Label(root, text=instruction, wraplength=300).pack(expand=True)
+        instruction = "Press NumLk (or alt+f2) after moving your cursor \n to a suitable position."
+        tk.Label(self.header, text=instruction, wraplength=300).pack(expand=True)
 
-        self.label = tk.Label(root, text="Log Label", wraplength=300)
-        self.label.pack()
+        # self.configs = {C_search.REGISTER_KEY: make_config_frame(C_search, self.body)}
+        self.configs = {KEY: make_config_frame(config, self.body) for KEY, config in _config_register.items()}
+        self.config_names = list(self.configs.keys())
+        self.current_config_name = self.config_names[0]
+        self.current_config_strvar = tk.StringVar(value=self.current_config_name)
+        tk.OptionMenu(self.body, self.current_config_strvar, *self.config_names, command=self.show_frame).pack(side="top", anchor="center", pady=10)
+        self.configs[self.current_config_name].pack(fill="both")
+
+        # self.label = tk.Label(self.body, text="Log Label"*20, wraplength=500)
+        # self.label.pack()
+     
+        self.update_root_geometry()
 
     def show_frame(self, name):
         self.configs[self.current_config_name].pack_forget()
         self.configs[name].pack(fill="both")
         self.current_config_name = name
-        self.root.update_idletasks()
+        self.update_root_geometry()
 
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    app = EditConfigsFrame(root)
-    root.mainloop()
+    tk_root = tk.Tk()
+    app = EditConfigsFrame(tk_root)
+    tk_root.mainloop()
